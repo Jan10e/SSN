7863699da0285e099b3060b1a405b19a89457574
Jantine <jantine.broek@yale.edu>
2018-04-10 20:07:41 -0400

    update axis

--- ssn_rate_noise_2D.m
+++ ssn_rate_noise_2D.m
@@ -20,8 +20,8 @@ W = [w_EE w_EI; w_IE w_II];
 
 
 % Membrane time constant 
-tau_E = 20/1000;                   %ms; 20ms for E
-tau_I = 10/1000;                    %ms; 10ms for I
+tau_E = 20000/1000;                   %ms; 20ms for E
+tau_I = 10000/1000;                    %ms; 10ms for I
 tau = [tau_E; tau_I];
 
 % Time vector
@@ -87,11 +87,11 @@ subplot(1,2,1)
 plot(h_range, mean_range, 'Linewidth', 1.5);
 legend("E","I", 'AutoUpdate','off')
 % Add a patch
-patch([1.5 3 3 1.5],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9]);
-patch([14 20 20 14],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9]);
+%patch([1.5 3 3 1.5],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9]);
+%patch([14 20 20 14],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9]);
 % The order of the "children" of the plot determines which one appears on top.
 % I need to flip it here.
-set(gca,'children',flipud(get(gca,'children')))
+%set(gca,'children',flipud(get(gca,'children')))
 % figure labels
 title("mean rate")
 ylabel("rate")
@@ -102,11 +102,11 @@ subplot(1,2,2)
 plot(h_range, stds_range, 'Linewidth', 1.5)
 legend("E","I", 'AutoUpdate','off')
 % Add a patch
-patch([1.5 3 3 1.5],[min(ylim) min(ylim) 2 2], [0.9 0.9 0.9])
-patch([14 20 20 14],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9])
+%patch([1.5 3 3 1.5],[min(ylim) min(ylim) 2 2], [0.9 0.9 0.9])
+%patch([14 20 20 14],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9])
 % The order of the "children" of the plot determines which one appears on top.
 % I need to flip it here.
-set(gca,'children',flipud(get(gca,'children')))
+%set(gca,'children',flipud(get(gca,'children')))
 % figure labels
 title("std dev. rate")
 ylabel("rate")
@@ -165,20 +165,23 @@ ncLow_hI = h_range(ncLow_idxI);
 % Find the values for parameter setting (h_tot) in arousal/locomotion and attention
 % h_tot = a * [1;b], a = h_range, b = I_range
 
-h_range = (2:1:20);
-I_range = (-3:0.2:3); %range for I cell parameter values
-par_change = zeros(length(h_range),length(I_range), 2, length(t));
+a_range = (0:1:20);
+b_range = (-3:0.2:3); %range for I cell parameter values
+par_change = zeros(length(h_range),length(b_range), 2, length(t));
 for a = 1:length(h_range)
     
     % update h_range input
     h_factor = h_range(a);
     fprintf('\n h-range: %d\n\n', h_factor)
     
-    for b = 1:length(I_range) % look over range of I input
+    for b = 1:length(b_range) % look over range of I input
+        
+        fprintf('\n b-value: %d\n', b_range(b))
     
-    % update I_range input    
-    h = [1;I_range(b)] * h_range(a);
-    fprintf('I-range: %d\n', h)
+        % update I_range input    
+         h = [1;b_range(b)] * h_range(a);
+         fprintf('E input: %d\n', h(1))
+         fprintf('I input: %d\n', h(2))
 
     
         %Generate noise vector
@@ -203,55 +206,86 @@ end
 mean_par = mean(par_change, 4);
 stds_par= std(par_change,0,4);
 
-save('data/par_change-b-3-3.mat', 'par_change')
-save('data/mean_par-b-3-3.mat', 'mean_par')
-save('data/stds_par-b-3-3.mat', 'stds_par')
+%save('data/par_change-b-3-3.mat', 'par_change')
+%save('data/mean_par-b-3-3.mat', 'mean_par')
+%save('data/stds_par-b-3-3.mat', 'stds_par')
 
 % plot stats
 figure;
 subplot(2,2,1)
 imagesc(mean_par(:,:,1))
 title("mean rate E")
-ylabel("h-range (0:1:20)")
-xlabel("I-range (0:0.1:5)")
+ylabel("a-range (0:1:20)")
+xlabel("b-range (-3:0.2:3)")
 colorbar
+
+xticklabels = b_range(1:3:end);
+xticks = linspace(1, size(mean_par(:,:,1), 2), numel(xticklabels));
+set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
+
+yticklabels = a_range(1:2:end);
+yticks = linspace(1, size(mean_par(:,:,1), 1), numel(yticklabels));
+set(gca, 'YTick', yticks, 'YTickLabel', flipud(yticklabels(:)))
+
 
 subplot(2,2,2)
 imagesc(mean_par(:,:,2))
 title("mean rate I")
-ylabel("h-range (0:1:20)")
-xlabel("I-range (0:0.1:5)")
+ylabel("a-range (0:1:20)")
+xlabel("b-range (-3:0.2:3)")
 colorbar
+
+xticklabels = b_range(1:3:end);
+xticks = linspace(1, size(mean_par(:,:,2), 2), numel(xticklabels));
+set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
+
+yticklabels = a_range(1:2:end);
+yticks = linspace(1, size(mean_par(:,:,2), 1), numel(yticklabels));
+set(gca, 'YTick', yticks, 'YTickLabel', flipud(yticklabels(:)))
+
 
 subplot(2,2,3)
 imagesc(stds_par(:,:,1))
 title("std dev rate E")
-ylabel("h-range (0:1:20)")
-xlabel("I-range (0:0.1:5)")
+ylabel("a-range (0:1:20)")
+xlabel("b-range (-3:0.2:3)")
 colorbar
+
+xticklabels = b_range(1:3:end);
+xticks = linspace(1, size(stds_par(:,:,1), 2), numel(xticklabels));
+set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
+
+yticklabels = a_range(1:2:end);
+yticks = linspace(1, size(stds_par(:,:,1), 1), numel(yticklabels));
+set(gca, 'YTick', yticks, 'YTickLabel', flipud(yticklabels(:)))
+
 
 subplot(2,2,4)
 imagesc(stds_par(:,:,2))
 title("std dev rate I")
-ylabel("h-range (0:1:20)")
-xlabel("I-range (0:0.1:5)")
+ylabel("a-range (0:1:20)")
+xlabel("b-range (-3:0.2:3)")
 colorbar
 
+xticklabels = b_range(1:3:end);
+xticks = linspace(1, size(stds_par(:,:,2), 2), numel(xticklabels));
+set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
 
-% what I-range gives the min std-dev over all h-ranges (look for lowest NC)
-%[stds_E_least, stds_E_least_idx] =min(stds_par(:,:,1)); %extract minimum std dev for I-range across h-range
-E_stds_b = mean(stds_par(:,:,1)); %get mean I-range values over h_range
+yticklabels = a_range(1:2:end);
+yticks = linspace(1, size(stds_par(:,:,2), 1), numel(yticklabels));
+set(gca, 'YTick', yticks, 'YTickLabel', flipud(yticklabels(:)))
+
+
+
+
+%% What I-range gives the min std-dev over all h-ranges (look for lowest NC)
+%get mean std dev of I-range values over h_range
+E_stds_b = mean(stds_par(:,:,1)); 
 
 figure;
 plot(E_stds_b, '-rs', 'LineWidth', 1.5)
 hold on
 ylabel('std dev')
-
-
-% beta_E = mean(stds_E_least_Irange); %get mean values
-% beta_E_min = stds_E_least_Irange(stds_E_least_Irange>0); %get ranges > min
-% beta_E_min = min(beta_E_min); %stds value 
-% [beta_E_max, beta_E_max_idx] = max(stds_E_least_Irange); %get ranges > max
 
 %get mean rate 
 E_rate_b = mean(mean_par(:,:,1));
@@ -268,7 +302,76 @@ E_diff = (E_rate_b(:) - E_stds_b(:));
 plot(E_diff, '-bs')
 
 [E_diff_max, E_diff_max_idx] = max(E_diff);
-I_range(E_diff_max_idx) %b value where nc and rate distance is largest 
+b_val = b_range(E_diff_max_idx); %b value where nc and rate distance is largest 
+
+
+%% Get attention and arousal state
+% plot mean and std dev of I and E population to check h_range * [1; b_val] (not yet: h_range * (a * [1;b_val]))
+
+h_range = (0:1:20);
+stds_range = zeros(2, length(h_range));
+mean_range = zeros(2, length(h_range));
+for a = 1:length(h_range)
+    
+    % update input
+    h_factor = h_range(a);
+    disp(h_factor)
+    h = [1;b_val] * h_factor; 
+
+    %Generate noise vector
+    for ii = 1:length(t)-1
+        eta(:,ii+1) = eta(:,ii) + (-eta(:,ii) *dt + sqrt(2 .*dt*tau_noise*sigma_a.^2).*(randn(2,1))) *(1./tau_noise);
+    end
+    
+
+    %Integrate neural system with noise forcing
+    for ii = 1: length(eta)-1  
+      % Forward Euler step + x(i) which is the noise
+      u(:,ii+1) = u(:,ii) + ode_rate(t, (u(:,ii)), h)*dt + eta(:,ii) * dt./tau; 
+    end
+  
+    
+     % Get mean and std
+    mean_range(:,a) = mean(u, 2);
+    stds_range(:,a)= std(u,0,2);
+    
+end
+
+figure;
+subplot(1,2,1)
+plot(h_range, mean_range, 'Linewidth', 1.5);
+legend("E","I", 'AutoUpdate','off')
+% Add a patch
+patch([1.5 3 3 1.5],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9]);
+patch([14 max(xlim) max(xlim) 14],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9]);
+% The order of the "children" of the plot determines which one appears on top.
+% I need to flip it here.
+set(gca,'children',flipud(get(gca,'children')))
+% figure labels
+title({"mean rate", "h_{range} * [1; b_{val}]"})
+ylabel("rate")
+xlabel("h")
+
+
+subplot(1,2,2)
+plot(h_range, stds_range, 'Linewidth', 1.5)
+legend("E","I", 'AutoUpdate','off')
+% Add a patch
+patch([1.5 3 3 1.5],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9])
+patch([14 max(xlim) max(xlim) 14],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9])
+% The order of the "children" of the plot determines which one appears on top.
+% I need to flip it here.
+set(gca,'children',flipud(get(gca,'children')))
+% figure labels
+title({"std dev. rate", "h_{range} * [1; b_{val}]"})
+ylabel("rate")
+xlabel("h")
+
+%saveas(gcf, [pwd '/figures/2Drate_meanstd_loc_03.png'])
+
+
+
+
 
 
 %% Get states
@@ -340,79 +443,3 @@ for m = 1:length(h_range)
     legend("E", "I")
 
 end
-
-
-%% Get mean and std dev of different h's
-h_range = (0:1:20);
-stds_range = zeros(2, length(h_range));
-mean_range = zeros(2, length(h_range));
-for a = 1:length(h_range)
-    
-    % update input
-    h_factor = h_range(a);
-    disp(h_factor)
-    %h = ones(2,1) * h_factor;
-    
-    %spontaneous
-    h = h_spont * h_factor;
-    
-    %stimulus only
-    %h = h_stim * h_factor;
-    
-    %attention
-    %h = h_att * h_factor;
-    
-    %locomotion
-   %h = h_loc * h_factor;
-    
-
-    %Generate noise vector
-    for ii = 1:length(t)-1
-        eta(:,ii+1) = eta(:,ii) + (-eta(:,ii) *dt + sqrt(2 .*dt*tau_noise*sigma_a.^2).*(randn(2,1))) *(1./tau_noise);
-    end
-    
-
-    %Integrate neural system with noise forcing
-    for ii = 1: length(eta)-1  
-      % Forward Euler step + x(i) which is the noise
-      u(:,ii+1) = u(:,ii) + ode_rate(t, (u(:,ii)), h)*dt + eta(:,ii) * dt./tau; 
-    end
-  
-    
-     % Get mean and std
-    mean_range(:,a) = mean(u, 2);
-    stds_range(:,a)= std(u,0,2);
-    
-end
-
-figure;
-subplot(1,2,1)
-plot(h_range, mean_range, 'Linewidth', 1.5);
-legend("E","I", 'AutoUpdate','off')
-% Add a patch
-patch([1.5 3 3 1.5],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9]);
-patch([14 max(xlim) max(xlim) 14],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9]);
-% The order of the "children" of the plot determines which one appears on top.
-% I need to flip it here.
-set(gca,'children',flipud(get(gca,'children')))
-% figure labels
-title({"mean rate",  "0.08 * [1;0.2]"})
-ylabel("rate")
-xlabel("h")
-
-
-subplot(1,2,2)
-plot(h_range, stds_range, 'Linewidth', 1.5)
-legend("E","I", 'AutoUpdate','off')
-% Add a patch
-patch([1.5 3 3 1.5],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9])
-patch([14 max(xlim) max(xlim) 14],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9])
-% The order of the "children" of the plot determines which one appears on top.
-% I need to flip it here.
-set(gca,'children',flipud(get(gca,'children')))
-% figure labels
-title({"std dev. rate", "0.08 * [1;0.2]"})
-ylabel("rate")
-xlabel("h")
-
-%saveas(gcf, [pwd '/figures/2Drate_meanstd_loc_03.png'])
