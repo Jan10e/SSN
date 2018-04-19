2533cce9c26e836594b523c699be8aad5ac03e8f
Jantine <jantine.broek@yale.edu>
2018-04-19 16:58:34 -0400

    correlation mtx

--- ssn_rate_noise_2D.m
+++ ssn_rate_noise_2D.m
@@ -9,7 +9,7 @@
 
 %% Parameters
 k = 0.3; %scaling constant 
-b = 2;
+n = 2;
 
 % Connectivity Matrix W as in Hennequin
 w_EE = 1.25;
@@ -29,7 +29,7 @@ dt = 1e-3;
 t = 0:dt:100;
 
 % Noise process is Ornstein-Uhlenbeck
-tau_noise = 50/1000; 
+tau_noise = 10/1000; 
 sigma_0E = 0.2;                      %mV; E cells
 sigma_0I = 0.1;                       %mV; I cells
 sigma_0 = [sigma_0E; sigma_0I]; %input noise std
@@ -46,7 +46,7 @@ u(:,1) = u_0;
 %% Functions
 
 % ODE with rate as dynamical variable
-ode_rate = @(t, u, h)  (-u + k.*ReLU(W *u + h).^b)./tau;
+ode_rate = @(t, u, h)  (-u + k.*ReLU(W *u + h).^n)./tau;
 
 
 %% Generate a graph of fluctuations versus input (noise can be excluded; line 101)
@@ -213,13 +213,15 @@ for b = 1:length(b_range)
     
 end
 
+%par_change_noTrans(:,:,:,:) = par_change(:,:,:,101:end);
+
 %stats using 4D matrix
 mean_par = mean(par_change, 4);
 stds_par= std(par_change,0,4);
 
-save('data/par_change-b-3-3.mat', 'par_change')
-save('data/mean_par-b-3-3.mat', 'mean_par')
-save('data/stds_par-b-3-3.mat', 'stds_par')
+%save('data/par_change-b-3-3-Tn10.mat', 'par_change')
+%save('data/mean_par-b-3-3-Tn10.mat', 'mean_par')
+%save('data/stds_par-b-3-3-Tn10.mat', 'stds_par')
 
 % plot stats
 figure;
@@ -354,85 +356,83 @@ set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 % set(gca, 'YTick', yticks, 'YTickLabel', flipud(yticklabels(:)))
 % 
 
-%% Check values
+%% Check values for similar input
 %CHECK: only plot input [1;1]
-b_range(21) 
+idx_b = find(b_range == 1);
 
 figure;
 subplot(2,2,1)
-imagesc(mean_par(21,:,1))
+imagesc(mean_par(idx_b,:,1))
 title("mean rate E")
 xlabel("a-range")
 ylabel("b-range")
 colorbar
 
 xticklabels = a_range(1:2:end);
-xticks = linspace(1, size(mean_par(:,:,1), 2), numel(xticklabels));
+xticks = linspace(1, size(mean_par(idx_b,:,1), 2), numel(xticklabels));
 set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
 
 yticklabels = b_range(21);
-yticks = linspace(1, size(mean_par(21,:,1), 1), numel(yticklabels));
+yticks = linspace(1, size(mean_par(idx_b,:,1), 1), numel(yticklabels));
 set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 
 
 subplot(2,2,2)
-imagesc(mean_par(21,:,2))
+imagesc(mean_par(idx_b,:,2))
 title("mean rate I")
 xlabel("a-range")
 ylabel("b-range")
 colorbar
 
 xticklabels = a_range(1:2:end);
-xticks = linspace(1, size(mean_par(:,:,2), 2), numel(xticklabels));
+xticks = linspace(1, size(mean_par(idx_b,:,2), 2), numel(xticklabels));
 set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
 
 yticklabels = b_range(21);
-yticks = linspace(1, size(mean_par(21,:,2), 1), numel(yticklabels));
+yticks = linspace(1, size(mean_par(idx_b,:,2), 1), numel(yticklabels));
 set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 
 
 subplot(2,2,3)
-imagesc(stds_par(21,:,1))
+imagesc(stds_par(idx_b,:,1))
 title("std dev rate E")
 xlabel("a-range")
 ylabel("b-range")
 colorbar
 
 xticklabels = a_range(1:2:end);
-xticks = linspace(1, size(stds_par(:,:,1), 2), numel(xticklabels));
+xticks = linspace(1, size(stds_par(idx_b,:,1), 2), numel(xticklabels));
 set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
 
 yticklabels = b_range(21);
-yticks = linspace(1, size(stds_par(21,:,1), 1), numel(yticklabels));
+yticks = linspace(1, size(stds_par(idx_b,:,1), 1), numel(yticklabels));
 set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 
 
 subplot(2,2,4)
-imagesc(stds_par(21,:,2))
+imagesc(stds_par(idx_b,:,2))
 title("std dev rate I")
 xlabel("a-range")
 ylabel("b-range")
 colorbar
 
 xticklabels = a_range(1:2:end);
-xticks = linspace(1, size(stds_par(:,:,2), 2), numel(xticklabels));
+xticks = linspace(1, size(stds_par(idx_b,:,2), 2), numel(xticklabels));
 set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
 
 yticklabels = b_range(21);
-yticks = linspace(1, size(stds_par(21,:,2), 1), numel(yticklabels));
+yticks = linspace(1, size(stds_par(idx_b,:,2), 1), numel(yticklabels));
 set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 
 
 
 %CHECK: look at the [1,1] input over the a-range. This should give the same
 %plots as before
-b_range(21)
-
 figure;
 subplot(1,2,1)
-plot(a_range,mean_par(21,:,1), 'LineWidth', 1.5);
+plot(a_range,mean_par(idx_b,:,1), 'LineWidth', 1.5);
 hold on
-plot(a_range,mean_par(21,:,2), 'LineWidth', 1.5)
+plot(a_range,mean_par(idx_b,:,2), 'LineWidth', 1.5)
 legend("E","I", 'AutoUpdate','off')
 title("mean rate")
 ylabel("rate")
@@ -440,20 +440,86 @@ xlabel("h")
 
 
 subplot(1,2,2)
-plot(a_range,stds_par(21,:,1), 'LineWidth', 1.5)
+plot(a_range,stds_par(idx_b,:,1), 'LineWidth', 1.5)
 hold on
-plot(a_range,stds_par(21,:,2), 'LineWidth', 1.5)
+plot(a_range,stds_par(idx_b,:,2), 'LineWidth', 1.5)
 legend("E","I", 'AutoUpdate','off')
 title("std dev. rate")
 ylabel("rate")
 xlabel("h")
 
 
+
+
+%% Cross correlogram
+% create cross corr for b = 1, and a = 2 & 15 to resembles Hennequin fig1E
+
+% a =2, E and I cells
+idx_b = b_range == 1;
+idx_a = a_range == 2;
+
+dat_a2 = squeeze(par_change(idx_b,idx_a,:,:))';
+dat_a2n = dat_a2 - mean(dat_a2,1); %substract mean value of time trace
+
+[corr_2,lags_2] = xcorr(dat_a2n, 'coeff');
+
+
+% a =15, E and I cells
+idx_a = a_range == 15;
+
+dat_a15 = squeeze(par_change(idx_b,idx_a,:,:))';
+dat_a15n = dat_a15 - mean(dat_a15,1); 
+
+[corr_15,lags_15] = xcorr(dat_a15n, 'coeff');
+
+
+titles = {'r_E - r_E','r_E - r_I','r_I - r_E','r_I - r_I'};
+figure;
+for row = 1:2
+    for col = 1:2
+        nm = 2*(row-1)+col;
+        subplot(2,2,nm)
+        stem(lags_2,corr_2(:,nm),'.')
+        hold on
+        stem(lags_15, corr_15(:,nm), '.')
+        %title(sprintf('E/I_{%d%d}',row,col))
+        title(titles{1,nm})
+        ylim([0 1])
+        xlim([-200 200])
+        ylabel('corr.')
+        xlabel('time lag (ms)')
+        legend('a=2', 'a=15')
+    end
+end
+
+
+%change tau noise and run it again to look for effect on cross corr
+% saved under: par_change-b-3-3-Tn10.mat
+
+
+
+
+% %Check: restrict calculations to lags between -0.2 and 0.2 seconds (200 ms)
+% [corr_2,lags_2] = xcorr(dat_a2,200,'coeff');
+% 
+% figure;
+% for row = 1:2
+%     for col = 1:2
+%         nm = 2*(row-1)+col;
+%         subplot(2,2,nm)
+%         stem(lags_2,corr_2(:,nm),'.')
+%         title(sprintf('E/I_{%d%d}',row,col))
+%         ylim([0 1])
+%         xlim([-200 200])
+%     end
+% end
+
+
 %% Indicate b-values that give a high std dev compared to b = 1
 % look at the mean value for std dev in the a-range 10-15
 
 %mean value for std dev for b=1 in a_range(10:15)
-b_range(21)
+find(b_range == 1)
 mean_std_1 = mean(stds_par(21,21:end,1));
 
 %mean value for std dev for all bs in a_range(10:15)
@@ -480,76 +546,76 @@ title('selection b-range')
 
 %% Look for b-value at the increased variability at h=2
 %ZOOM: only plot values for a_range =< 5 (look at stds)
-a_range(11) 
+idx_a = find(a_range == 5);
 
 figure;
 subplot(2,2,1)
-imagesc(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,1))
+imagesc(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,1))
 title("mean rate E")
 xlabel("a-range")
 ylabel("b-range")
 colorbar
 
 xticklabels = a_range(1:2:11);
-xticks = linspace(1, size(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,1), 2), numel(xticklabels));
+xticks = linspace(1, size(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,1), 2), numel(xticklabels));
 set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
 
 yticklabels = b_range(mean_std_sm(1,2):mean_std_sm(end,2));
-yticks = linspace(1, size(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,1), 1), numel(yticklabels));
+yticks = linspace(1, size(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,1), 1), numel(yticklabels));
 set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 
 
 subplot(2,2,2)
-imagesc(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,2))
+imagesc(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,2))
 title("mean rate I")
 xlabel("a-range")
 ylabel("b-range")
 colorbar
 
 xticklabels = a_range(1:2:11);
-xticks = linspace(1, size(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,2), 2), numel(xticklabels));
+xticks = linspace(1, size(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,2), 2), numel(xticklabels));
 set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
 
 yticklabels = b_range(mean_std_sm(1,2):mean_std_sm(end,2));
-yticks = linspace(1, size(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,2), 1), numel(yticklabels));
+yticks = linspace(1, size(mean_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,2), 1), numel(yticklabels));
 set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 
 
 subplot(2,2,3)
-imagesc(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,1))
+imagesc(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,1))
 title("std dev rate E")
 xlabel("a-range")
 ylabel("b-range")
 colorbar
 
 xticklabels = a_range(1:2:11);
-xticks = linspace(1, size(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,1), 2), numel(xticklabels));
+xticks = linspace(1, size(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,1), 2), numel(xticklabels));
 set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
 
 yticklabels = b_range(mean_std_sm(1,2):mean_std_sm(end,2));
-yticks = linspace(1, size(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,1), 1), numel(yticklabels));
+yticks = linspace(1, size(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,1), 1), numel(yticklabels));
 set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 
 
 subplot(2,2,4)
-imagesc(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,2))
+imagesc(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,2))
 title("std dev rate I")
 xlabel("a-range")
 ylabel("b-range")
 colorbar
 
 xticklabels = a_range(1:2:11);
-xticks = linspace(1, size(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,2), 2), numel(xticklabels));
+xticks = linspace(1, size(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,2), 2), numel(xticklabels));
 set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
 
 yticklabels = b_range(mean_std_sm(1,2):mean_std_sm(end,2));
-yticks = linspace(1, size(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:11,2), 1), numel(yticklabels));
+yticks = linspace(1, size(stds_par(mean_std_sm(1,2):mean_std_sm(end,2),1:idx_a,2), 1), numel(yticklabels));
 set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 
 
 
 %CHECK: look at individual inputs [...,...] to check with 2D plot
-idx_b = 11;
+idx_b = find(b_range == -1); %change what to find to look at different plots
 b_range(idx_b)
 
 figure;
@@ -744,7 +810,7 @@ set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 figure;
 xticklabels = a_range2(1:4:end);
 xticks = linspace(1, size(mean_par2(:,:,1), 2), numel(xticklabels));
-yticklabels = b_range2(1:4:end);
+yticklabels = b_range2(1:2:end);
 yticks = linspace(1, size(mean_par2(:,:,1), 1), numel(yticklabels));
 
 subplot(2,2,1)
@@ -781,247 +847,69 @@ set(gca, 'XTick', xticks, 'XTickLabel', xticklabels)
 set(gca, 'YTick', yticks, 'YTickLabel', yticklabels)
 
 
+%% Plot b = (0.55:0.7) vs b = 1
 
+%b-range2 1 is
+b_range2(17)
 
+%b-range2 0.55, 0.6, 0.65, 0.7 is idx 8, 9, 10, 11
+b_range2(8)
 
-
-
-
-
-
-
-
-
-
-
-%% Parameter search CHECK
-a_range = (0:1:20);
-%b_range = (-3:0.2:3); %range for I cell parameter values
-par_change_c = zeros(length(a_range), 2, length(t));
-%for b = 1:length(b_range)
-    
-    % update h_range input
-    %fprintf('\n b-range: %d\n\n', b_range(b))
-    
-    for a = 1:length(a_range) % look over range of I input
-        
-        fprintf('\n a-value: %d\n', a_range(a))
-    
-        % update I_range input    
-         h = [1;1] * a_range(a);
-         fprintf('E input: %d\n', h(1))
-         fprintf('I input: %d\n', h(2))
-
-    
-        %Generate noise vector
-        for ii = 1:length(t)-1
-            eta(:,ii+1) = eta(:,ii) + (-eta(:,ii) *dt + sqrt(2 .*dt*tau_noise*sigma_a.^2).*(randn(2,1))) *(1./tau_noise);
-        end
-
-        %Integrate neural system with noise forcing
-        for ii = 1: length(eta)-1
-            % Forward Euler step + x(i) which is the noise
-            u(:,ii+1) = u(:,ii) + ode_rate(t, (u(:,ii)), h)*dt + eta(:,ii) * dt./tau; 
-        end
-     
-            % add u to matrix 
-            par_change_c(a,:,:) = u;
-        
-    end
-    
-%end
-
-%stats
-mean_par_c = mean(par_change_c, 3);
-stds_par_c= std(par_change_c,0,3);
-
+num = [17,8,9,10,11];
 
 figure;
-subplot(2,2,1)
-imagesc(mean_par_c(:,1))
-colorbar
-
-subplot(2,2,2)
-imagesc(mean_par_c(:,2))
-colorbar
-
-subplot(2,2,3)
-imagesc(stds_par_c(:,1))
-colorbar
-
-subplot(2,2,4)
-imagesc(stds_par_c(:,2))
-colorbar
-
-
-figure;
-plot(a_range,mean_par_c(:,1))
-hold on
-plot(a_range,mean_par_c(:,2))
-figure;
-plot(a_range,stds_par_c(:,1))
-hold on
-plot(a_range,stds_par_c(:,2))
-
-
-%% What I-range gives the min std-dev over all h-ranges (look for lowest NC)
-%get mean std dev of I-range values over h_range
-E_stds_b = mean(stds_par(:,:,1)); 
-
-figure;
-plot(E_stds_b, '-rs', 'LineWidth', 1.5)
-hold on
-ylabel('std dev')
-
-%get mean rate 
-E_rate_b = mean(mean_par(:,:,1));
-
-plot(E_rate_b, 'g', 'LineWidth', 2)
-hold on
-xlabel('idx I-range')
-ylabel('rate')
-title('b-param for E population')
-legend("std dev", "rate")
-
-%look where distance between nc and rate is largest
-E_diff = (E_rate_b(:) - E_stds_b(:));
-plot(E_diff, '-bs')
-
-[E_diff_max, E_diff_max_idx] = max(E_diff);
-b_val = b_range(E_diff_max_idx); %b value where nc and rate distance is largest 
-
-
-%% Get attention and arousal state
-% plot mean and std dev of I and E population to check h_range * [1; b_val] (not yet: h_range * (a * [1;b_val]))
-
-h_range = (0:1:20);
-stds_range = zeros(2, length(h_range));
-mean_range = zeros(2, length(h_range));
-for a = 1:length(h_range)
+for n=1:length(num)
     
-    % update input
-    h_factor = h_range(a);
-    disp(h_factor)
-    h = [1;b_val] * h_factor; 
+    idx = num(n);
 
-    %Generate noise vector
-    for ii = 1:length(t)-1
-        eta(:,ii+1) = eta(:,ii) + (-eta(:,ii) *dt + sqrt(2 .*dt*tau_noise*sigma_a.^2).*(randn(2,1))) *(1./tau_noise);
-    end
-    
-
-    %Integrate neural system with noise forcing
-    for ii = 1: length(eta)-1  
-      % Forward Euler step + x(i) which is the noise
-      u(:,ii+1) = u(:,ii) + ode_rate(t, (u(:,ii)), h)*dt + eta(:,ii) * dt./tau; 
-    end
-  
-    
-     % Get mean and std
-    mean_range(:,a) = mean(u, 2);
-    stds_range(:,a)= std(u,0,2);
-    
-end
-
-figure;
-subplot(1,2,1)
-plot(h_range, mean_range, 'Linewidth', 1.5);
-legend("E","I", 'AutoUpdate','off')
-% Add a patch
-patch([1.5 3 3 1.5],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9]);
-patch([14 max(xlim) max(xlim) 14],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9]);
-% The order of the "children" of the plot determines which one appears on top.
-% I need to flip it here.
-set(gca,'children',flipud(get(gca,'children')))
-% figure labels
-title({"mean rate", "h_{range} * [1; b_{val}]"})
-ylabel("rate")
-xlabel("h")
-
-
-subplot(1,2,2)
-plot(h_range, stds_range, 'Linewidth', 1.5)
-legend("E","I", 'AutoUpdate','off')
-% Add a patch
-patch([1.5 3 3 1.5],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9])
-patch([14 max(xlim) max(xlim) 14],[min(ylim) min(ylim) max(ylim) max(ylim)], [0.9 0.9 0.9])
-% The order of the "children" of the plot determines which one appears on top.
-% I need to flip it here.
-set(gca,'children',flipud(get(gca,'children')))
-% figure labels
-title({"std dev. rate", "h_{range} * [1; b_{val}]"})
-ylabel("rate")
-xlabel("h")
-
-%saveas(gcf, [pwd '/figures/2Drate_meanstd_loc_03.png'])
-
-
-%% Get states
-
-% h_spont 
-%is where the nc is highest, ncHigh, which is at h = 2
-%constant is ncHigh_hE as in the vector the value for E is 1
-ncHigh = ncHigh_hE;
-
-% get value for I in vector
-%I_spont = ncHigh_hI/ncHigh_hE;
-%h_spont = ncHigh*[1;I_spont];
-h_spont = 2 * [1;1];
-
-
-%h_stim 
-%is where nc is lowest, ncLow, which is at h = 15
-%constant is ncLow_hE as in the vector the value for E is 1
-ncLow = ncLow_hE;
-
-%get value for I in vector
-% I_stim = ncLow_hI/ncLow_hE;
-% h_stim = ncLow * [1;I_stim];
-h_stim = 15 * [1;1];
-
-
-%h_att
-%NC decreases more for h_stim, so less fluctuation in rates and evoked rates
-%increase (h > 15)
-alpha = 0.07; %should be smaller than lambda
-h_att = alpha * [1;0.2]; %play around with I value
-
-
-%h_loc
-% NC decreases more for h_stim, so less fluctuation in rates and evoked rates
-%increase (h > 15). This is a large effect compared to h_att
-% lambda = 0.3; %0.3 - 0.5 gives decreasing nc and relatively low sc, but all nc are higher than normal
-% h_loc = lambda * [1;0.2]; %play around with I value: [1; 0.2] seems best
-
-lambda = 0.5; %0.5 - 0.7 gives stable or increasing rate
-h_loc = lambda * [1;1.1]; 
-
-
-%% Rate output for h is h_spont, h_stim, h_att, h_loc
-figure;
-h_range = [h_spont, h_stim, h_att, h_loc];
-for m = 1:length(h_range)
-    
-    % update input
-    h = h_range(:,m);
-    disp(h)
-    
-    %Generate noise vector
-    for ii = 1:length(t)-1
-        eta(:,ii+1) = eta(:,ii) + (-eta(:,ii) *dt + sqrt(2 .*dt*tau_noise*sigma_a.^2).*(randn(2,1))) *(1./tau_noise);
-    end
-    
-    %Integrate neural system with noise forcing
-    for ii = 1: length(eta)-1  
-      % Take the Euler step + x(i) which is the noise
-      u(:,ii+1) = u(:,ii) + ode_rate(t, (u(:,ii)), h)*dt + eta(:,ii) * dt./tau; 
-    end
-    
-    subplot(1, 4, m)
-    plot(t, u, 'Linewidth',1) %order plots: h_spont, h_stim, h_att, h_loc
-    ylim([0 100])
+    %E only
+    subplot(1,2,1)
+    plot(a_range2(:),mean_par2(idx,:,1), 'LineWidth', 1);
+    hold on
+    legend(strcat('b =', num2str(b_range2(num)')))
+    title("mean rate E")
     ylabel("rate")
-    xlabel("time")
-    legend("E", "I")
+    xlabel("a-range")
 
+    subplot(1,2,2)
+    plot(a_range2(:),stds_par2(idx,:,1), 'LineWidth', 1)
+    hold on
+    legend(strcat('b =', num2str(b_range2(num)')))
+    title("std dev. rate E")
+    ylabel("rate")
+    xlabel("a-range")
 end
+
+
+figure;
+for n=1:length(num)    
+    
+    idx = num(n);
+    
+    %I only
+    subplot(1,2,1)
+    plot(a_range2(:),mean_par2(idx,:,2), 'LineWidth', 1)
+    hold on
+    legend(strcat('b =',num2str(b_range2(num)')))
+    title("mean rate I")
+    ylabel("rate")
+    xlabel("a-range")
+
+    subplot(1,2,2)
+    plot(a_range2(:),stds_par2(idx,:,2), 'LineWidth', 1)
+    hold on
+    legend(strcat('b =', num2str(b_range2(num)')))
+    title("std dev. rate I")
+    ylabel("rate")
+    xlabel("a-range")
+end
+
+
+
+
+
+
+
+
+
+
